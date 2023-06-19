9336805128b7dbbc8f4b1d58f51625c81b6b88b4
Tanya Matseonzhek <matseonzhek@gmail.com>
2023-06-19 23:42:40 +0400

    added servlet init in Main

--- module-main/src/main/java/com/mjc/school/Main.java
+++ module-main/src/main/java/com/mjc/school/Main.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
@@ -13,13 +15,12 @@ import java.util.Collections;
 
 @EntityScan(basePackageClasses = {Main.class, Jsr310JpaConverters.class})
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
     public static void main(String[] args) {
 
         SpringApplication application = new SpringApplication(Main.class);
         application.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
         application.run(args);
-//        SpringApplication.run(Main.class, args);
     }
 
     @Bean
@@ -29,5 +30,8 @@ public class Main {
                 .build();
     }
 
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Main.class);
+    }
 }
